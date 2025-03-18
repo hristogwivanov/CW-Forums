43598d72d41ddd634f1bf13bd4327a86a95ab487
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-03-18 14:47:13 +0200

    React router dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+    <Header />
+    <main id="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
+    <Footer />
     </>
   )
 }
