133e977a10604179799d0938ef2fd28c721e0e55
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-03-18 15:35:28 +0200

    Header construction started

---
+++ src/layout/header.jsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css";
+
+export const Header = () => {
+    return (
+        <header>
+            <h1 className={styles.header}>Header</h1>
+        </header>
+    )
+}
