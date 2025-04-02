63751ea0f9ae6fe4537eedbe463535513e92f5ff
Hristo Ivanov <gw.hivanov@gmail.com>
2025-04-02 21:16:52 +0300

    Fixed footer position issue in deployed version

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,9 +1,9 @@
-import{ Link } from "react-router";
+import { Link } from "react-router-dom";
 import styles from "./footer.module.css";
 
 export const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <Link to="/about">
                 <div><p>Hristo Ivanov 2025</p></div>
             </Link>
